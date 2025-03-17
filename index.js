25942e6e01d90ed57d99076ddbf835c95547f9f3
AuroraHumo <carles.casallachs@gmail.com>
2025-03-17 19:40:25 +0100

    añadir 2a api chistes random

--- index.js
+++ index.js
@@ -15,78 +15,118 @@ let report2 = document.getElementById('report2');
 let report3 = document.getElementById('report3');
 let acuditActual;
 let reportAcudits = [];
-let summary = document.getElementById("summary");
+//let summary = document.getElementById("summary") as HTMLElement;
 let temperature = document.getElementById("temperature");
 let wind = document.getElementById("wind");
 let cloud_cover = document.getElementById("cloud_cover");
+const API_NINJAS_KEY = 'ZD0Du+VPhzXbg3gWaAGo6A==bz9YNUdjll0ezWkZ';
 button.addEventListener('click', generarBroma);
-window.addEventListener('DOMContentLoaded', generarBroma);
-window.addEventListener('DOMContentLoaded', getWeather);
+window.addEventListener('DOMContentLoaded', () => {
+    generarBroma();
+    getWeather();
+});
 report1.addEventListener('click', function () {
-    report('1', acuditActual);
+    report(1, acuditActual);
 });
 report2.addEventListener('click', function () {
-    report('2', acuditActual);
+    report(2, acuditActual);
 });
 report3.addEventListener('click', function () {
-    report('3', acuditActual);
+    report(3, acuditActual);
 });
 const url = 'https://www.meteosource.com/api/v1/free/point?lat=41.38879&lon=2.15899&sections=current&timezone=auto&language=en&units=metric&key=e1pppqt2rqwmkh0usnqcj7rj9d9zy1mt6xy40uob';
+/* async function getWeather() {
+    try {
+        const resposta = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json'
+            }
+        });
+
+        if (!resposta.ok) {
+            throw new Error(`Error: ${resposta.status}`);
+        }
+
+        const dades = await resposta.json();
+
+        summary.innerText = dades.current.summary;
+        temperature.innerText = `${dades.current.temperature}°C`;
+        wind.innerText = ` 🌬️ ${dades.current.wind.speed} km/h (${dades.current.wind.dir})`;
+        cloud_cover.innerText = ` ☁️ Cloudyness: ${dades.current.cloud_cover}%`;
+
+    } catch (error) {
+        console.error("No s'ha pogut obtenir la informació del temps:", error);
+    }
+} */
 function getWeather() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             const resposta = yield fetch(url, {
                 method: 'GET',
-                headers: {
-                    'Accept': 'application/json'
-                }
+                headers: { 'Accept': 'application/json' }
             });
-            if (!resposta.ok) {
+            if (!resposta.ok)
                 throw new Error(`Error: ${resposta.status}`);
-            }
             const dades = yield resposta.json();
-            summary.innerText = dades.current.summary;
-            temperature.innerText = `${dades.current.temperature}°C`;
-            wind.innerText = `🌬️ ${dades.current.wind.speed} km/h (${dades.current.wind.dir})`;
-            cloud_cover.innerText = `☁️ Cloudyness: ${dades.current.cloud_cover}%`;
+            //summary.innerText = dades.current.summary;
+            temperature.innerText = ` BCN: ${dades.current.temperature}°C `;
+            wind.innerText = `-  ${dades.current.wind.speed} km/h (${dades.current.wind.dir}) `;
+            cloud_cover.innerText = `-  Cloudyness: ${dades.current.cloud_cover}%`;
         }
         catch (error) {
             console.error("No s'ha pogut obtenir la informació:", error);
         }
     });
 }
+function getRandomJokeAPI() {
+    const apis = [
+        'https://icanhazdadjoke.com/',
+        'https://api.api-ninjas.com/v1/jokes'
+    ];
+    return apis[Math.floor(Math.random() * apis.length)];
+}
 function generarBroma() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            const respuesta = yield fetch('https://icanhazdadjoke.com/', {
-                headers: { 'Accept': 'application/json' }
-            });
-            if (!respuesta.ok) {
-                throw new Error(`Something went wrong:' ${respuesta.status}`);
+            const apiURL = getRandomJokeAPI();
+            const headers = { 'Accept': 'application/json' };
+            if (apiURL.includes('api-ninjas.com')) {
+                headers['X-Api-Key'] = API_NINJAS_KEY;
             }
-            const json = yield respuesta.json();
-            resultDiv.innerHTML = json.joke;
-            acuditActual = json.joke;
-            return acuditActual;
+            const resposta = yield fetch(apiURL, { headers });
+            if (!resposta.ok) {
+                throw new Error(`Something went wrong: ${resposta.status}`);
+            }
+            const json = yield resposta.json();
+            let joke = '';
+            if (apiURL.includes('icanhazdadjoke')) {
+                joke = json.joke;
+            }
+            else if (apiURL.includes('api-ninjas.com')) {
+                joke = json[0].joke;
+            }
+            resultDiv.innerHTML = joke;
+            acuditActual = joke;
         }
         catch (error) {
-            resultDiv.innerHTML = 'Dad is not joking today:';
+            resultDiv.innerHTML = 'Error. Dad is not joking today...';
+            console.error(error);
         }
     });
 }
 function report(rate, acuditActual) {
     let date = new Date().toISOString();
-    let score = parseInt(rate);
     let found = reportAcudits.find((item) => {
         return item.joke === acuditActual;
     });
     if (found) {
-        found.score = score;
+        found.score = rate;
     }
     else {
         reportAcudits.push({
             'joke': acuditActual,
-            'score': score,
+            'score': rate,
             'date': date
         });
     }
--- index.ts
+++ index.ts
@@ -7,25 +7,29 @@ let report3 = document.getElementById('report3') as HTMLButtonElement;
 let acuditActual: string;
 let reportAcudits: { joke: string; score: number; date: string; }[] = []
 
-let summary = document.getElementById("summary") as HTMLElement;
+//let summary = document.getElementById("summary") as HTMLElement;
 let temperature = document.getElementById("temperature") as HTMLElement;
 let wind = document.getElementById("wind") as HTMLElement;
 let cloud_cover = document.getElementById("cloud_cover") as HTMLElement;
 
+const API_NINJAS_KEY = 'ZD0Du+VPhzXbg3gWaAGo6A==bz9YNUdjll0ezWkZ';
+
 button.addEventListener('click', generarBroma)
-window.addEventListener('DOMContentLoaded', generarBroma);
-window.addEventListener('DOMContentLoaded', getWeather);
+window.addEventListener('DOMContentLoaded', () => {
+    generarBroma();
+    getWeather();
+});
 
 report1.addEventListener('click', function() {
-    report('1', acuditActual)});
+    report(1, acuditActual)});
 report2.addEventListener('click', function() {
-    report('2', acuditActual)});
+    report(2, acuditActual)});
 report3.addEventListener('click', function() {
-    report('3', acuditActual)});
+    report(3, acuditActual)});
 
 const url = 'https://www.meteosource.com/api/v1/free/point?lat=41.38879&lon=2.15899&sections=current&timezone=auto&language=en&units=metric&key=e1pppqt2rqwmkh0usnqcj7rj9d9zy1mt6xy40uob';
 
-async function getWeather() {
+/* async function getWeather() {
     try {
         const resposta = await fetch(url, {
             method: 'GET',
@@ -42,47 +46,89 @@ async function getWeather() {
 
         summary.innerText = dades.current.summary;
         temperature.innerText = `${dades.current.temperature}°C`;
-        wind.innerText = `🌬️ ${dades.current.wind.speed} km/h (${dades.current.wind.dir})`;
-        cloud_cover.innerText = `☁️ Cloudyness: ${dades.current.cloud_cover}%`;
+        wind.innerText = ` 🌬️ ${dades.current.wind.speed} km/h (${dades.current.wind.dir})`;
+        cloud_cover.innerText = ` ☁️ Cloudyness: ${dades.current.cloud_cover}%`;
 
     } catch (error) {
-        console.error("No s'ha pogut obtenir la informació:", error);
+        console.error("No s'ha pogut obtenir la informació del temps:", error);
     }
+} */
+    async function getWeather() {
+        try {
+            const resposta = await fetch(url, {
+                method: 'GET',
+                headers: { 'Accept': 'application/json' }
+            });
+    
+            if (!resposta.ok) throw new Error(`Error: ${resposta.status}`);
+    
+            const dades = await resposta.json();
+    
+            //summary.innerText = dades.current.summary;
+            temperature.innerText = ` BCN: ${dades.current.temperature}°C `;
+            wind.innerText = `-  ${dades.current.wind.speed} km/h (${dades.current.wind.dir}) `;
+            cloud_cover.innerText = `-  Cloudyness: ${dades.current.cloud_cover}%`;
+        } catch (error) {
+            console.error("No s'ha pogut obtenir la informació:", error);
+        }
+    }
+
+function getRandomJokeAPI(): string {
+    const apis = [
+        'https://icanhazdadjoke.com/',
+        'https://api.api-ninjas.com/v1/jokes'
+
+    ];
+    return apis[Math.floor(Math.random() * apis.length)];
 }
 
 async function generarBroma() {
     try {
-        const respuesta = await fetch ('https://icanhazdadjoke.com/', {
-            headers: { 'Accept': 'application/json' }
-        })
-        if (!respuesta.ok) {
-            throw new Error (`Something went wrong:' ${respuesta.status}`)
+        const apiURL = getRandomJokeAPI();
+        const headers: HeadersInit = { 'Accept': 'application/json' };
+
+        if (apiURL.includes('api-ninjas.com')) {
+            headers['X-Api-Key'] = API_NINJAS_KEY;
         }
-        const json = await respuesta.json()
-        resultDiv.innerHTML = json.joke
-        acuditActual = json.joke
-        return acuditActual
+
+        const resposta = await fetch(apiURL, { headers });
+
+        if (!resposta.ok) {
+            throw new Error(`Something went wrong: ${resposta.status}`);
+        }
+
+        const json = await resposta.json();
+        
+        let joke = '';
+        if (apiURL.includes('icanhazdadjoke')) {
+            joke = json.joke;
+        } else if (apiURL.includes('api-ninjas.com')) {
+            joke = json[0].joke;
+        }
+
+        resultDiv.innerHTML = joke;
+        acuditActual = joke;
 
     } catch (error) {
-        resultDiv.innerHTML ='Dad is not joking today:'
+        resultDiv.innerHTML = 'Error. Dad is not joking today...';
+        console.error(error);
     }
 }
 
-function report(rate: string, acuditActual: string) {
+function report(rate: number, acuditActual: string) {
     
     let date = new Date().toISOString()
-    let score = parseInt(rate)
 
     let found = reportAcudits.find((item) => {
         return item.joke === acuditActual;
     });
 
     if (found) {
-        found.score = score;
+        found.score = rate;
     } else {
         reportAcudits.push({
             'joke': acuditActual,
-            'score': score,        
+            'score': rate,        
             'date': date
         })
     }
